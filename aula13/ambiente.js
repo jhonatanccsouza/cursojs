32385206ffdcca3eb8a912fb81be474d43cdb77e
jhonatanccsouza <jhonatan.github@gmail.com>
2025-07-09 13:03:53 -0300

    Create ambiente.js

---
+++ aula13/ambiente.js
@@ -0,0 +1,40 @@
+console.log('Vai começar...')
+for(var c = 1; c <= 5 ; c++){
+    console.log(`Passo ${c}`)
+
+}
+console.log('Terminou')
+
+
+
+/*
+var contador = 1
+
+do{
+
+    console.log(`Passo ${contador}`)
+    contador++
+
+
+} while (contador <=500)
+
+*/
+
+/*
+var contador = 1
+while (contador <=500){
+    console.log(`Passo ${contador}`)
+    contador++
+}
+*/
+
+
+/*
+console.log("Tudo Bem?")
+console.log("Tudo Bem?")
+console.log("Tudo Bem?")
+console.log("Tudo Bem?")
+console.log("Tudo Bem?")
+console.log("Tudo Bem?")
+*/
+
